00f7b0c0347830327d855fda4ba0d8262d59d3b8
pinheiro09x <135390139+pinheiro09x@users.noreply.github.com>
2023-12-11 20:22:10 -0300

    Add files via upload

---
+++ ex7.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main() {
+  float nt1, nt2, nt3, media, turma = 0, mediaturma;
+  int contador = 0;
+
+  while (contador < 30) {
+    printf("Por favor, digite suas notas da prova um, dois e três respectivamente: ");
+    scanf("%f %f %f", &nt1, &nt2, &nt3);
+
+    media = (nt1 * 2 + nt2 * 4 + nt3 * 3) / 10;
+    printf("\nSua média foi igual a: %.2f", media);
+
+    if (media >= 7) {
+      printf("\nAprovado\n");
+    } else {
+      printf("\nReprovado\n");
+    }
+
+    contador++;
+    turma += media;
+  }
+
+  mediaturma = turma / contador;
+  printf("\nA média da turma é igual a %.2f\n", mediaturma);
+
+  return 0;
+}
